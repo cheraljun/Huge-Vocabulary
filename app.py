70fcc4d6e8b751ce08e4d3aecaeb9ba117714873
jun <1773384983@qq.com>
2025-09-25 15:59:37 +0800

    8200

--- app.py
+++ app.py
@@ -649,9 +649,10 @@ if __name__ == "__main__":
                     t.start()
     except Exception:
         pass
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    # from waitress import serve
 
-    from waitress import serve
-
-    serve(app, host="0.0.0.0", port=5000)
+    # serve(app, host="0.0.0.0", port=5000)
 
 
